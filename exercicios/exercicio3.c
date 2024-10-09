648d1ecc7f30bb722c182ad3dc10773de4f258e8
Caroline Morelli da Silveira <caroline_ms2004@hotmail.com>
2024-10-09 09:14:18 -0300

    fix: Variável de estado - exercicio3

--- exercicios/exercicio3.c
+++ exercicios/exercicio3.c
@@ -1,13 +1,14 @@
 int ledPin = 13; 
 int buttonPin = 7;  
-int buttonState = 0;    
+int estadoBotao = 0; 
+
 void setup() {
-    pinMode(ledPin, OUTPUT);  //13 saida
-    pinMode(buttonPin, INPUT); //7 entrada
+    pinMode(ledPin, OUTPUT);  //13 - saida
+    pinMode(buttonPin, INPUT); //7 - entrada
 }
 
 void loop() {
-    buttonState = digitalRead(buttonPin); 
+    estadoBotao = digitalRead(buttonPin); 
     
     if (estadoBotao == HIGH) {  //botao pressionado
         digitalWrite(ledPin, HIGH); 
